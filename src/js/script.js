3861fb835605c4af9a14a54718ea7bb5a664d706
ChristianoDc7 <christianod97@gmail.com>
2021-10-20 10:25:26 -0400

    Tableau vita niampy select region ok

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,206 @@
 
 var tableau = document.querySelector('.table')
 var obj;
-function getvalues(axian){
+
+
+
+function tablu(deb,fin){
+    if(deb){
+    let ddu = Number(deb.substr(-2,2))
+    let mdu = Number(deb.substr(-5,2))
+    let dfu = Number(fin.substr(-2,2))
+    let mfu = Number(fin.substr(-5,2))
+    let mth ;
+
+    //switch du debut 
+    switch (mdu) {
+        case 10:
+           if(ddu>0 && ddu<11){
+               mth = 0
+           }else if(ddu>10 && ddu<21){
+                mth = 1
+           }else{
+                mth = 2
+           };
+          break;
+        case 11:
+            if(ddu>0 && ddu<11){
+                mth = 3
+            }else if(ddu>10 && ddu<21){
+                 mth = 4
+            }else{
+                 mth = 5
+            };
+          break;
+        case 12:
+            if(ddu>0 && ddu<11){
+                mth = 6
+            }else if(ddu>10 && ddu<21){
+                 mth = 7
+            }else{
+                 mth = 8
+            };
+          break;
+        case 1:
+            if(ddu>0 && ddu<11){
+                mth = 9
+            }else if(ddu>10 && ddu<21){
+                 mth = 10
+            }else{
+                 mth = 11
+            };
+          break;
+        case 2:
+            if(ddu>0 && ddu<11){
+                mth = 12
+            }else if(ddu>10 && ddu<21){
+                 mth = 13
+            }else{
+                 mth = 14
+            };
+          break;
+        case 3:
+            if(ddu>0 && ddu<11){
+                mth = 15
+            }else if(ddu>10 && ddu<21){
+                 mth = 16
+            }else{
+                 mth = 17
+            };                    
+          break;
+        case 4:
+            if(ddu>0 && ddu<11){
+                mth = 18
+            }else if(ddu>10 && ddu<21){
+                 mth = 19
+            }else{
+                 mth = 20
+            };
+          break
+        case 5:
+            if(ddu>0 && ddu<11){
+                mth = 21
+            }else if(ddu>10 && ddu<21){
+                 mth = 22
+            }else{
+                 mth = 23
+            };
+          break
+        default :
+         mth = "tsisy"
+
+      }
+
+      let msh;
+      //switch du fin
+      switch (mfu) {
+        case 10:
+           if(dfu>0 && dfu<11){
+               msh = 0
+           }else if(dfu>10 && dfu<21){
+                msh = 1
+           }else{
+                msh = 2
+           };
+          break;
+        case 11:
+            if(dfu>0 && dfu<11){
+                msh = 3
+            }else if(dfu>10 && dfu<21){
+                 msh = 4
+            }else{
+                 msh = 5
+            };
+          break;
+        case 12:
+            if(dfu>0 && dfu<11){
+                msh = 6
+            }else if(dfu>10 && dfu<21){
+                 msh = 7
+            }else{
+                 msh = 8
+            };
+          break;
+        case 1:
+            if(dfu>0 && dfu<11){
+                msh = 9
+            }else if(dfu>10 && dfu<21){
+                 msh = 10
+            }else{
+                 msh = 11
+            };
+          break;
+        case 2:
+            if(dfu>0 && dfu<11){
+                msh = 12
+            }else if(dfu>10 && dfu<21){
+                 msh = 13
+            }else{
+                 msh = 14
+            };
+          break;
+        case 3:
+            if(dfu>0 && dfu<11){
+                msh = 15
+            }else if(dfu>10 && dfu<21){
+                 msh = 16
+            }else{
+                 msh = 17
+            };                    
+          break;
+        case 4:
+            if(dfu>0 && dfu<11){
+                msh = 18
+            }else if(dfu>10 && dfu<21){
+                 msh = 19
+            }else{
+                 msh = 20
+            };
+          break
+        case 5:
+            if(dfu>0 && dfu<11){
+                msh = 21
+            }else if(dfu>10 && dfu<21){
+                 msh = 22
+            }else{
+                 msh = 23
+            };
+          break
+        default :
+         msh = "tsisy"
+
+      }
+      
+  
+    let derp = ['<td></td>','<td></td>','<td></td>','<td></td>','<td></td>','<td></td>','<td></td>','<td></td>','<td></td>','<td></td>','<td></td>','<td></td>','<td></td>','<td></td>','<td></td>','<td></td>','<td></td>','<td></td>','<td></td>','<td></td>','<td></td>','<td></td>','<td></td>','<td></td>',]
+
+    for(let i=mth; i<msh ; i++){
+    derp[i]='<td class="blue"></td>'}
+    derp[msh]='<td class="red"></td>'
+    return derp.join('')}else{
+        return ""
+    }
+}
+function getvalues(fambolena,faritra){
+    if(faritra=="none" && fambolena=="none"){
+        document.getElementById('geer').innerHTML = "<h1>Misafidiana sokajy azafady!!</h1>"
+    }else if(faritra=="none" && fambolena!="none"){
+        fetch('src/database/data.json')
+        .then((response) =>response.json())
+        .then((data)=>data.filter(({culture}) => culture.includes(fambolena)))
+        .then((data) => generate(data))
+    }else if(faritra!="none" && fambolena=="none"){
+        fetch('src/database/data.json')
+        .then((response) =>response.json())
+        .then((data)=>data.filter(({Region}) => Region.includes(faritra)))
+        .then((data) => generate(data))
+    }else{
+        fetch('src/database/data.json')
+        .then((response) =>response.json())
+        .then((data)=>data.filter(({culture,Region}) => culture.includes(fambolena) && Region.includes(faritra)))
+        .then((data) => generate(data))
+    }
     
-    fetch('src/database/data.json')
-    .then((response) =>response.json())
-    .then((data)=>data.filter(({culture}) => culture.includes(axian)))
-    .then((data) => generate(data))
     
 }
 function generate(resp){
@@ -160,7 +354,7 @@ function generate(resp){
         <td colspan="2" class="colonne1">
             Préparation du sol
         </td>
-        <td></td><td></td><td class="blue"></td><td class="blue"></td><td class="red"></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+        ${tablu(element.Prepa_sol_start,element.prepa_sol_end)}
     </tr>`}else{
          prepa=""
     }
@@ -176,7 +370,7 @@ function generate(resp){
         <td colspan="2" class="colonne1">
             Semis/plantation
         </td>
-        <td></td><td></td><td></td><td></td><td></td><td class="blue"></td><td class="blue"></td><td class="blue"></td><td class="red"></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+        ${tablu(element.semis_start,element.semi_end)}
     </tr>`}else {
         semis=""
     }
@@ -190,7 +384,7 @@ function generate(resp){
         <td colspan="2" class="colonne1">
             Repiquage
         </td>
-        <td></td><td></td><td></td><td></td><td></td><td class="blue"></td><td class="blue"></td><td class="blue"></td><td class="red"></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+        ${tablu(element.rep_start,element.rep_end)}
     </tr>`}else {
         repi=""
     }
@@ -217,19 +411,19 @@ function generate(resp){
         <td class="colonne1">
             1er
         </td>
-        <td></td><td></td><td class="blue"></td><td class="blue"></td><td class="red"></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+        ${tablu(element.fert_1_sart,element.fert_1_end)}
     </tr>
     <tr>
         <td class="colonne1">
             2ème
         </td>
-        <td></td><td></td><td></td><td></td><td></td><td></td><td class="blue"></td><td class="blue"></td><td class="blue"></td><td class="red"></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+        ${tablu(element.fert_2_start,element.fert_2_end)}
     </tr>
 <tr ${cls}>
     <td class="colonne1" >
         3ème
     </td>
-    <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td class="blue"></td><td class="blue"></td><td class="blue"></td><td class="red"></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+    ${tablu(element.fert_3_start,element.fert_3_end)}
 </tr>`}else{
     fert=""
 }
@@ -242,9 +436,12 @@ if (`${element.sarc_1_start }`!="null"){
         if(`${element.sarc_2_start}`=="null"){
             j=""
             karc="style='display:none'"
+           
         }else{
             j=2
             karc=""
+            
+            
         };
     sarc = `<tr>
        <td rowspan="${j}" class="colonne1">
@@ -253,14 +450,14 @@ if (`${element.sarc_1_start }`!="null"){
         <td class="colonne1">
             1er
         </td>
-        <td></td><td></td><td></td><td></td><td></td><td></td><td class="blue"></td><td class="blue"></td><td class="blue"></td><td class="red"></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+        ${tablu(element.sarc_1_start,element.sarc_1_end)}
     </tr>
     <tr ${karc}>
         <td class="colonne1" >
             2ème
         </td>
-        <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td class="blue"></td><td class="blue"></td><td class="blue"></td><td class="red"></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
-    </tr>
+        ${tablu(element.sarc_2_start,element.sarc_2_end)}
+    </tr> 
     `}else{
         sarc=""
     }
@@ -286,13 +483,13 @@ if (`${element.but_1_start }`!="null"){
         <td class="colonne1" >
             1er
         </td>
-        <td></td><td></td><td></td><td></td><td></td><td></td><td class="blue"></td><td class="blue"></td><td class="blue"></td><td class="red"></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+        ${tablu(element.but_1_start,element.but_1_end)}
     </tr>
     <tr ${larc}>
         <td class="colonne1">
             2ème
         </td>
-        <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td class="blue"></td><td class="blue"></td><td class="blue"></td><td class="red"></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+        ${tablu(element.but_2_start,element.but_2_end)}
     </tr>
     `}else{
         butt =""
@@ -306,12 +503,12 @@ let recl = `<tr>
         <td colspan="2" class="colonne1">
             Recolte
         </td>
-        <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td class="blue"></td><td class="blue"></td><td class="blue"></td>
+        ${tablu(element.rec_start,element.rec_end)}
     </tr>
 </table>`
 
 
-        return tabs + prepa + semis  + fert + sarc + butt + recl
+        return tabs + prepa + semis  +repi+ fert + sarc + butt + recl    
     }).join('')
     document.getElementById('geer').innerHTML = html
 }
